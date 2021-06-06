ac1152efd0a381e418bc6db5d51e2daabb736dbe
Ana onablo <79449819+onablo@users.noreply.github.com>
2021-06-06 18:59:03 +0300

    Nd4+

--- 05/nd4Masyvai3.php
+++ 05/nd4Masyvai3.php
@@ -55,27 +55,88 @@ Sudėkite masyvus, sudėdami atitinkamas reikšmes.
 Paskaičiuokite kiek unikalių (po vieną, nesikartojančių) 
 reikšmių ir kiek unikalių kombinacijų gavote. */
 
-$size = 20;
-for ($i = 0; $i < $size; $i++) {
+for ($i = 0; $i <= 20; $i++) {
     $masyvas1[] = $letters[rand(0, 3)];
     $masyvas2[] = $letters[rand(0, 3)];
     $masyvas3[] = $letters[rand(0, 3)];    
 }
 
-for ($i = 0; $i < $size; $i++) {
-    $masyvasCount = $masyvas1[$i] . $masyvas2[$i] . $masyvas3[$i];
+print_r($masyvas1);
+echo '<br>';
+echo '---------------------------<br>';
+print_r($masyvas2);
+echo '<br>';
+echo '---------------------------<br>';
+print_r($masyvas3);
+echo '<br>';
+echo '---------------------------<br>';
+
+$masyvasAbc = [];
+foreach ($masyvas1 as $i => $value ) {
+    $naujaReiksme = $masyvas1[$i] . $masyvas2[$i] . $masyvas3[$i];
+    array_push($masyvasAbc, $naujaReiksme);
 }
-print_r($masyvasCount);
+print_r($masyvasAbc);
+echo '<br>';
+echo '---------------------------<br>';
 
+$unikAbcMasyvas = array_unique($masyvasAbc);
+print_r($unikAbcMasyvas);
+echo '<br>';
+echo '---------------------------<br>';
 
+$sumaReiksmiuMasyvasAbc = count($masyvasAbc);
+$sumaUnikReiksmiu = 0;
+$sumaNeunikReiksmiu = 0;
+foreach ($unikAbcMasyvas as $i => $value ) {
+    $sumaUnikReiksmiu  = count($unikAbcMasyvas);
+    $sumaNeunikReiksmiu = $sumaReiksmiuMasyvasAbc - $sumaUnikReiksmiu;
+}
+echo "Unikaliu reiksmiu masyve suma yra: $sumaUnikReiksmiu, 
+neunikaliu: $sumaNeunikReiksmiu. ";
+echo '<br>';
 
 
 
 echo '<hr>';
 echo '</div>';
 echo '<br>';
-echo 'Nr5<br>';
+echo 'Nr6<br>';
 echo '-------<br>';
-/* 6. Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai 
+/* 6. Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai     _nepabaigtas
 skaičiai nuo 100 iki 999. Masyvų ilgiai 100. Masyvų reikšmės turi 
 būti unikalios savo masyve (t.y. neturi kartotis). */
+
+$masyvas1 = [];
+foreach(range(1, 100) as $i => $val) {
+    $masyvas1[] = rand(100, 999);
+    $unikMasyvas1 = array_unique($masyvas1);
+}
+
+$sumaUnikMasyvas1  = count($unikMasyvas1);
+print_r($unikMasyvas1);
+echo " =  $sumaUnikMasyvas1.";
+echo '<br>';
+echo '---------------------------<br>';
+
+
+
+$masyvas2 = [];
+foreach(range(1, 100) as $i => $val) {
+    $masyvas2[] = rand(100, 999);
+    $unikMasyvas2 = array_unique($masyvas2);
+}
+$sumaUnikMasyvas2  = count($masyvas2);
+print_r($unikMasyvas2);
+echo " =  $sumaUnikMasyvas2.";
+echo '<br>';
+echo '---------------------------<br>';
+
+echo '<hr>';
+echo '</div>';
+echo '<br>';
+echo 'Nr7<br>';
+echo '-------<br>';
+/*  7. Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios
+ yra pirmame 6 uždavinio masyve, bet nėra antrame 6 uždavinio masyve. */
+
